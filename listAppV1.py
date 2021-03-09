12755ca1d8f08d9475bcda766c32195fd002ccf4
leslievg <79866108+leslievg@users.noreply.github.com>
2021-03-09 12:20:23 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,31 @@
+"""
+Program goals:
+1. Add items to a list (ints)
+2. Offer the user a choice of actions
+3. Pull the values stored at specific indexes
+4. Convert input to INTs
+
+"""
+def mainProgram():
+    print("hello, there! Let's work with lists!")
+    print("please choose from the following options. Type the number of the choice")
+    choice = input("1. Add to a list, 2. Return a valuea at a list    ")
+    if choice == "1";
+       addToList()
+    elif choice == "2":
+        indexValues()
+
+def addToList():
+    print("adding to a list! Great Choice!")
+    newIteam = input("type an integer here!  ")
+    myList.append(int(newIteam))
+    #we need to think about errors!
+
+def indxValues():
+
+
+
+
+if __name__ == "__main__":
+    mainProgram()
+
