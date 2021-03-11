883ebe71aacfccc3078aff545850bdd4004f5129
leslievg <79866108+leslievg@users.noreply.github.com>
2021-03-11 11:32:07 -0700

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -4,16 +4,24 @@ Program goals:
 2. Offer the user a choice of actions
 3. Pull the values stored at specific indexes
 4. Convert input to INTs
+5. Put all action in a while loop
+6. Add an exit option
 
 """
+
+myList = []
 def mainProgram():
-    print("hello, there! Let's work with lists!")
-    print("please choose from the following options. Type the number of the choice")
-    choice = input("1. Add to a list, 2. Return a valuea at a list    ")
-    if choice == "1";
-       addToList()
-    elif choice == "2":
-        indexValues()
+    #build our while loop
+    while True:
+        print("hello, there! Let's work with lists!")
+        print("please choose from the following options. Type the number of the choice")
+        choice = input("1. Add to a list, 2. Return a valuea at a list    ")
+        if choice == "1";
+           addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "3":
+            break
 
 def addToList():
     print("adding to a list! Great Choice!")
@@ -21,8 +29,11 @@ def addToList():
     myList.append(int(newIteam))
     #we need to think about errors!
 
-def indxValues():
-
+def indexValues():
+    print("At what index position do you  want to search?")
+    indexPos = input( "Type an index position here:    ")
+    print (myList[int(indexPos)])
+    
 
 
 
