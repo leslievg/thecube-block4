2f5c2067676d4f6a12bcf498fc5bd432b3014b0c
leslievg <79866108+leslievg@users.noreply.github.com>
2021-04-23 11:07:14 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -41,6 +41,8 @@ def mainProgram():
         elif choice == "8":
             searchIteam = input("What are you looking for?   ")
             recursiveBinarySearch(unique_list, 0, len(unique_list)-1,int(searchItem))
+        elif choice == "9":
+            JumpSearch=
         else:
             break
 
@@ -50,13 +52,16 @@ def addToList():
     myList.append(int(newIteam))
     #we need to think about errors!
 
-def addAvunch():
+""
+
+def addABunch():
     print ("we're going to add a bunch of numbers to your list!")
     numToAdd = input("How many new integers would you like to add?  ")
     numRang = input("And how high would you like these numbers to go?  ")
     for x in range(0,int(numToAdd)):
         myList.append(random.ranint(0,  int(numrange)))
     print("Your list is complete!")
+""
 
 
 def sortList(myList):
@@ -69,10 +74,14 @@ def sortList(myList):
     if showMe.lower() =="Y":
         print(unique_list)
 
+""
+
 
 def randomSearch ():
     print("oH, iM sO rAnDoM!!!")
     print(myList[random.randint(0,  len(myList)-1)])
+
+""
 
 
 def linearearch():
@@ -81,6 +90,31 @@ def linearearch():
     for x in range (len(myList)):
         if myList[x] == int(searchValue):
             print(" Your item is at index position {}".format (x))
+
+""
+
+
+def JumpSearch (lys, val):
+    lenght = len(lys)
+    jump = int(math.sqrt(length))
+    left, right = 0, 0
+    while left < lenght and lys[left] <=
+    val:
+        right = min(lenght -1, left=ju mp)
+        if lys [left] <= val and lys[right] >= val:
+            break
+        left += jump;
+      if left >= lenght or lys [left] > val:
+          return -1
+        right = min(lenght -1, right)
+        i = left
+        whilei <= right and lys [i] <= val:
+            iflys[i] == val:
+                return i
+            i += 1
+          return -1
+
+""
 
 
 def recursiveBinarySearch(unique_list, low, high, x):
@@ -95,6 +129,9 @@ def recursiveBinarySearch(unique_list, low, high, x):
             return recursiveBinarySearch(unique_list, mid +1, high, x)
     else:
         print("Your number isn't here!")
+
+" recursve binary search is an implemation of the binary search algorithm that uss recursive method calls(instead of iteratively searchingfor the iteam with a single method call"
+
 
 
 def iterativeBinarySearch(unique_list, X):
@@ -112,6 +149,9 @@ def iterativeBinarySearch(unique_list, X):
         else :
              return mid
         return -1
+
+" If number at position mid equal to key or target element then the control returns index of mid element by printing that the number has been found along with the index at witch it was found"
+
              
     
         
@@ -119,7 +159,7 @@ def indexValues():
     print("At what index position do you  want to search?")
     indexPos = input( "Type an index position here:    ")
     print(myList[int(indexPos)])
-
+""
 
 
 def printList ():
@@ -129,6 +169,8 @@ def printList ():
         wichOne = input("Wich listdo you want to see? Sorted or un-sorted ?  ")
         if wichOne.lower() == "sorted"
              print (unique_list)
+
+""
  
 
 
